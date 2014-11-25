d713978d42addc743611221e69aa97a114ed787f
hawkrobe <rxdh@stanford.edu>
2014-11-24 23:09:47 -0800

    tagged some exp1 data

--- experiment1/analysis/Q_and_A_exp1.py
+++ experiment1/analysis/Q_and_A_exp1.py
@@ -6,8 +6,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import statsmodels.formula.api as sm
-from sklearn.linear_model import LinearRegression
 %pylab inline
 # Make things looks pretty
 pd.set_option('display.max_colwidth', 1000)
@@ -40,6 +38,7 @@ df.insert(2, "domain", domain_col)
 
 df['q_explanation'] = df['q_explanation'].str.replace('&quotechar', '\'', case=False)
 df['a_explanation'] = df['a_explanation'].str.replace('&quotechar', '\'', case=False)
+df['q_exp_tag'] = df['q_exp_tag'].str.strip()
 
 # <markdowncell>
 
@@ -51,16 +50,49 @@ df = df.drop(['workerid','trial_type'], 1)
 
 # <codecell>
 
-df.sort_index(by = ['set_relation', 'domain'] )
+df.sort_index(by = ['domain','set_relation'] )
 
 # <codecell>
 
-df.groupby('set_relation').agg(['count','mean',np.std])
+tab = df.groupby('set_relation').agg(['count','mean',np.std])
+print(df.groupby(['domain', 'q_exp_tag']).agg(['count']))['helpful']
+pr_prop = [21/float(25), 22/float(25), 1/float(25), 24/float(25)]
 
 # <codecell>
 
-df.boxplot(column = 'helpful', by = 'set_relation')
-plt.show()
+
+unique(df['domain'])
+
+# <codecell>
+
+for domain in unique(df['domain']) :
+    sub_df = df[df['domain'] == domain]
+    sub_df.boxplot(column = 'helpful', by = 'set_relation')
+    plt.title(domain)
+    plt.show()
+
+# <codecell>
+
+import nltk
+from nltk.corpus import stopwords
+minlength = 2
+porter = nltk.PorterStemmer()
+global_list = []
+for i in range(len(df['q_explanation'])) :
+    sentence = df['q_explanation'][i]
+    new_list = []
+    mysentencetokens_sw= nltk.word_tokenize(sentence)
+    mysentencetokens = [token for token in mysentencetokens_sw if 
+                        (not token in stopwords.words('english')) and len(token) >= minlength]
+    for token in mysentencetokens :
+        print(porter.stem(token))
+        new_list.append(porter.stem(token))
+    global_list.append(new_list)
+df['parsed_q'] = global_list
+
+# <codecell>
+
+df['parsed_q']
 
 # <codecell>
 
