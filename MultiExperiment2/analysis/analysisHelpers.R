753765d8c8ea3169b34dae71f3e17e51fd78a74d
hawkrobe <rxdh@stanford.edu>
2015-10-02 14:36:11 -0700

    refactor analysis helpers to take vals instead of rows

--- MultiExperiment2/analysis/analysisHelpers.R
+++ MultiExperiment2/analysis/analysisHelpers.R
@@ -11,42 +11,42 @@ overlappingNearLeftAnswers <- c("house cat", "hotel lobby", "lettuce", "metal ch
 overlappingNearRightAnswers <- c("lion", "diner", "carrots", "iron skillet")
 overlappingFarRightAnswers <- c("whale", "park", "flower", "wooden ruler")
 
-mapAnswer <- function(row) {
-  if(row$type == "equivocal") {
-    if(row$answer %in% equivocalFarLeftAnswers)
+mapAnswer <- function(type, answer) {
+  if(type == "equivocal") {
+    if(answer %in% equivocalFarLeftAnswers)
       return("A1")
-    else if(row$answer %in% equivocalNearLeftAnswers)
+    else if(answer %in% equivocalNearLeftAnswers)
       return("A2")
-    else if(row$answer %in% equivocalNearRightAnswers)
+    else if(answer %in% equivocalNearRightAnswers)
       return("A3")
-    else if(row$answer %in% equivocalFarRightAnswers)
+    else if(answer %in% equivocalFarRightAnswers)
       return("A4")
     else 
-      stop(cat("Unrecognized answer in equivocal:", row$answer))
-  } else if(row$type == "branching") {
-    if(row$answer %in% branchingFarLeftAnswers)
+      stop(cat("Unrecognized answer in equivocal:", answer))
+  } else if(type == "branching") {
+    if(answer %in% branchingFarLeftAnswers)
       return("A1")
-    else if(row$answer %in% branchingNearLeftAnswers)
+    else if(answer %in% branchingNearLeftAnswers)
       return("A2")
-    else if(row$answer %in% branchingNearRightAnswers)
+    else if(answer %in% branchingNearRightAnswers)
       return("A3")
-    else if(row$answer %in% branchingFarRightAnswers)
+    else if(answer %in% branchingFarRightAnswers)
       return("A4")
     else 
-      stop(cat("Unrecognized answer in branching:", row$answer))
-  } else if(row$type == "overlapping") {
-    if(row$answer %in% overlappingFarLeftAnswers)
+      stop(cat("Unrecognized answer in branching:", answer))
+  } else if(type == "overlapping") {
+    if(answer %in% overlappingFarLeftAnswers)
       return("A1")
-    else if(row$answer %in% overlappingNearLeftAnswers)
+    else if(answer %in% overlappingNearLeftAnswers)
       return("A2")
-    else if(row$answer %in% overlappingNearRightAnswers)
+    else if(answer %in% overlappingNearRightAnswers)
       return("A3")
-    else if(row$answer %in% overlappingFarRightAnswers)
+    else if(answer %in% overlappingFarRightAnswers)
       return("A4")
     else 
-      stop(cat("Unrecognized answer in overlapping: ", row$answer))
+      stop(cat("Unrecognized answer in overlapping: ", answer))
   } else {
-    stop(cat("Unrecognized type", row$type))
+    stop(cat("Unrecognized type", type))
   }
 }
 
@@ -61,76 +61,76 @@ overlappingDominatedQuestions <- c("restaurant", "cat", "food", "metal thing")
 overlappingNonDominatedQuestions <- c("bar", "pet", "leaves", "seat")
 overlappingHighestLevelQuestions <- c("place", "animal", "plant", "thing")
 
-mapQuestion <- function(row) {
-  if(row$type == "equivocal") {
-    if(row$question %in% equivocalLeftQuestions) {
+mapQuestion <- function(type, question) {
+  if(type == "equivocal") {
+    if(question %in% equivocalLeftQuestions) {
       return("Q1")
-    } else if(row$question %in% equivocalRightQuestions)
+    } else if(question %in% equivocalRightQuestions)
       return("Q2")
     else 
-      stop(cat("Unrecognized question in equivocal:", row$question))
-  } else if(row$type == "branching") {
-    if(row$question %in% branchingLowestLevelQuestions)
+      stop(cat("Unrecognized question in equivocal:", question))
+  } else if(type == "branching") {
+    if(question %in% branchingLowestLevelQuestions)
       return("Q1")
-    else if(row$question %in% branchingNextLowestLevelQuestions)
+    else if(question %in% branchingNextLowestLevelQuestions)
       return("Q2")
-    else if(row$question %in% branchingNextHighestLevelQuestions)
+    else if(question %in% branchingNextHighestLevelQuestions)
       return("Q3")
-    else if(row$question %in% branchingHighestLevelQuestions)
+    else if(question %in% branchingHighestLevelQuestions)
       return("Q4")
     else 
-      stop(cat("Unrecognized question in branching:", row$question))
-  } else if(row$type == "overlapping") {
-    if(row$question %in% overlappingLowestLevelQuestions)
+      stop(cat("Unrecognized question in branching:", question))
+  } else if(type == "overlapping") {
+    if(question %in% overlappingLowestLevelQuestions)
       return("Q1")
-    else if(row$question %in% overlappingDominatedQuestions)
+    else if(question %in% overlappingDominatedQuestions)
       return("Q2")
-    else if(row$question %in% overlappingNonDominatedQuestions)
+    else if(question %in% overlappingNonDominatedQuestions)
       return("Q3")
-    else if(row$question %in% overlappingHighestLevelQuestions)
+    else if(question %in% overlappingHighestLevelQuestions)
       return("Q4")
     else 
-      stop(cat("Unrecognized question in overlapping: ", row$question))
+      stop(cat("Unrecognized question in overlapping: ", question))
   } else {
-    stop(cat("Unrecognized type", row$type))
+    stop(cat("Unrecognized type", type))
   }
 }
 
-mapGoal <- function(row) {
-  if(row$type == "equivocal") {
-    if(row$goal %in% equivocalFarLeftAnswers)
+mapGoal <- function(type, goal) {
+  if(type == "equivocal") {
+    if(goal %in% equivocalFarLeftAnswers)
       return("G1")
-    else if(row$goal %in% equivocalNearLeftAnswers)
+    else if(goal %in% equivocalNearLeftAnswers)
       return("G2")
-    else if(row$goal %in% equivocalNearRightAnswers)
+    else if(goal %in% equivocalNearRightAnswers)
       return("G3")
-    else if(row$goal %in% equivocalFarRightAnswers)
+    else if(goal %in% equivocalFarRightAnswers)
       return("G4")
     else 
-      stop(cat("Unrecognized answer in equivocal:", row$goal))
-  } else if(row$type == "branching") {
-    if(row$goal %in% branchingFarLeftAnswers)
+      stop(cat("Unrecognized answer in equivocal:", goal))
+  } else if(type == "branching") {
+    if(goal %in% branchingFarLeftAnswers)
       return("G1")
-    else if(row$goal %in% branchingNearLeftAnswers)
+    else if(goal %in% branchingNearLeftAnswers)
       return("G2")
-    else if(row$goal %in% branchingNearRightAnswers)
+    else if(goal %in% branchingNearRightAnswers)
       return("G3")
-    else if(row$goal %in% branchingFarRightAnswers)
+    else if(goal %in% branchingFarRightAnswers)
       return("G4")
     else 
-      stop(cat("Unrecognized answer in branching:", row$goal))
-  } else if(row$type == "overlapping") {
-    if(row$goal %in% overlappingFarLeftAnswers)
+      stop(cat("Unrecognized answer in branching:", goal))
+  } else if(type == "overlapping") {
+    if(goal %in% overlappingFarLeftAnswers)
       return("G1")
-    else if(row$goal %in% overlappingNearLeftAnswers)
+    else if(goal %in% overlappingNearLeftAnswers)
       return("G2")
-    else if(row$goal %in% overlappingNearRightAnswers)
+    else if(goal %in% overlappingNearRightAnswers)
       return("G3")
-    else if(row$goal %in% overlappingFarRightAnswers)
+    else if(goal %in% overlappingFarRightAnswers)
       return("G4")
     else 
-      stop(cat("Unrecognized answer in overlapping: ", row$goal))
+      stop(cat("Unrecognized answer in overlapping: ", goal))
   } else {
-    stop(cat("Unrecognized type", row$type))
+    stop(cat("Unrecognized type", type))
   }
 }
