fc683ff684d23bd750df89dfd1892f7d787b08e2
Robert Hawkins <rxdh@stanford.edu>
2019-01-07 16:01:18 -0800

    Make waiting room message part of player class

--- experiments/SpatialExperiment/src/player.js
+++ experiments/SpatialExperiment/src/player.js
@@ -3,7 +3,10 @@ class GamePlayer {
     this.instance = playerInstance;
     this.game = gameInstance;
     this.role = '';
-    this.message = '';
+    this.message = '<p>Waiting for another player...<br/>  \
+                       Please do not refresh the page!<br /> \
+                       If wait exceeds 5 minutes, we recommend returning the HIT \
+                       and trying again later.</p>';
     this.id = '';
   }
 };
