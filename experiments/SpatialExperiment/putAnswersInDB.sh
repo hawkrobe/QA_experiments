8a0fe98738b5cd4452f6c3f9c7ded8450393f94b
Robert Hawkins <hawkrobe@gmail.com>
2019-02-11 20:58:54 -0800

    shell script to stick json of model output in mongo

---
+++ experiments/SpatialExperiment/putAnswersInDB.sh
@@ -0,0 +1,1 @@
+mongoimport --db QA --collection answererBotResponses --authenticationDatabase admin --username <user> --password <password> --drop --jsonArray --file ./src/spatialAnswererOutput.json
